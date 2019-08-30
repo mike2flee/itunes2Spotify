a75cce01c762f770bb7e1786ce784a26b37cf367
mike2flee <Micjj78@gmail.com>
2019-08-30 00:07:48 -0400

    ADDING HAS ERROR: Attempted import error: 'hasHistory' is not exported from 'react-router'.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,21 @@ import React from "react";
 import { Provider } from "react-redux";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Router, hasHistory } from "react-router";
+import { syncHistoryWithStore } from "react-router-redux";
 import configureStore from "./configureStore";
+import routes from "./routes";
 import * as serviceWorker from "./serviceWorker";
 
 const store = configureStore();
+const history = syncHistoryWithStore(hasHistory, store);
 ReactDOM.render(
   <Provider store={store}>
-    <App></App>
+    <div>
+      <div>
+        <Router history={history} routes={routes} />
+      </div>
+    </div>
   </Provider>,
   document.getElementById("root")
 );
---
+++ src/routes.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { Route, IndexRedirect } from "react-router";
+import App from "./App";
+
+export default <Route path="/" component={App}></Route>;
