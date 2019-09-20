1d8d9329720afa942325918398c33399f5136c8f
Lotun16 <38048224+Lotun16@users.noreply.github.com>
2019-09-19 20:10:37 -0400

    Located Songs

--- src/common/constants.js
+++ src/common/constants.js
@@ -3,8 +3,8 @@ const uriConstants = {
   CLIENT_SECRET: "ab192b2513fc4557b82f7e681bc1fda0",
   // REDIRECTURL:
   //  "http://itunes2spotify-20190826194208-hostingbucket-dev.s3-website-us-east-1.amazonaws.com/",
-  // REDIRECTURL: "http://localhost:3000/playListCreation",
-  REDIRECTURL: "https://murmuring-plains-67099.herokuapp.com/playListCreation",
+  REDIRECTURL: "http://localhost:3000/playListCreation",
+  //REDIRECTURL: "https://murmuring-plains-67099.herokuapp.com/playListCreation",
   FULL_PLAYLIST_AND_TRACK_MOD:
     "playlist-modify-public%20playlist-modify-private%20user-library-modify"
 };
--- src/webScraper.js
+++ src/webScraper.js
@@ -3,21 +3,32 @@ import { Button } from "reactstrap";
 const request = require("request");
 const cheerio = require("cheerio");
 
+var Song = function(songName, artist){
+  this.songName = songName; 
+  this.artist = artist;
+}
+
 class webScraper extends React.Component {
     constructor(props){
         super(props);
-        this.setState = {
+        this.state = {
             htmlBody: "Enter Dev mode to view loaded cheerio object",
             link: "https://music.apple.com/us/playlist/summer-2019/pl.u-b3b88DNfkj9gYd"
         };
         this.iTunesDom = this.iTunesDom.bind(this);
 
     }
+
     iTunesDom() {
         request(this.state.link, function(error, response, html) {
           if (!error && response.statusCode === 200) {
             const $ = cheerio.load(html);
-            console.log($("*"));
+            const playlistName = $('.product-header__title');
+            const userName = $('.product-header__identity');
+            const tracks = $('.tracklist-item__text');
+            console.log(playlistName.html()); 
+            console.log(userName.html());
+            console.log(tracks.text());
           }
         });
         this.setState({
